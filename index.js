3bc79365ee35ae07675e195bb9f864dc6ae1b66f
Sam Power <sam.g.power@gmail.com>
2019-03-18 21:50:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name) {
+    return drivers.filter(name => name);
+}
+
